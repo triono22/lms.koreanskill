0fd120a31ebb2b0d73d06f8905cddaa76c6a763f
Admin PT Songmun KoreanSkill <admin@songmunkorean.com>
2026-05-30 11:06:21 +0900

    Simplify user creation in admin

--- users/admin.py
+++ users/admin.py
@@ -2,17 +2,22 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from .models import User
+from .forms import AdminUserCreationForm
 
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
     """Custom User admin with role field."""
+    add_form = AdminUserCreationForm
 
-    list_display = ("username", "email", "role", "is_staff", "is_active")
+    list_display = ("username", "first_name", "last_name", "role", "is_staff", "is_active")
     list_filter = ("role", "is_staff", "is_active")
     fieldsets = BaseUserAdmin.fieldsets + (
         ("LMS Role", {"fields": ("role",)}),
     )
-    add_fieldsets = BaseUserAdmin.add_fieldsets + (
-        ("LMS Role", {"fields": ("role",)}),
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('first_name', 'last_name', 'tanggal_lahir', 'role'),
+        }),
     )
--- users/forms.py
+++ users/forms.py
@@ -21,3 +21,40 @@ class ProfileEditForm(forms.ModelForm):
                 'placeholder': 'email@example.com'
             }),
         }
+
+class AdminUserCreationForm(forms.ModelForm):
+    """Custom form for creating users in the admin panel easily."""
+    tanggal_lahir = forms.DateField(
+        label="Tanggal Lahir",
+        help_text="Gunakan date picker. Akan dijadikan password default dengan format DDMMYYYY.",
+        widget=forms.DateInput(attrs={'type': 'date'})
+    )
+    
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'role')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['first_name'].required = True
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        
+        # Generate username dari nama depan
+        first_name_clean = self.cleaned_data['first_name'].lower().replace(' ', '')
+        base_username = first_name_clean
+        counter = 1
+        while User.objects.filter(username=base_username).exists():
+            base_username = f"{first_name_clean}{counter}"
+            counter += 1
+        user.username = base_username
+        
+        # Set password dari tanggal lahir (DDMMYYYY)
+        tanggal_lahir = self.cleaned_data['tanggal_lahir']
+        password = tanggal_lahir.strftime('%d%m%Y')
+        user.set_password(password)
+        
+        if commit:
+            user.save()
+        return user
